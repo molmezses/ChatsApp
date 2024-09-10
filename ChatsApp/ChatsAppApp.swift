0dcce1ee98ba017fd4ae75c00685ab6c45249663
Mustafa <mustafaolmezses@icloud.com>
2024-09-11 00:11:52 +0300

    Initial Commit

---
+++ ChatsApp/ChatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsAppApp.swift
+//  ChatsApp
+//
+//  Created by Mustafa Ölmezses on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
